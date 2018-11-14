dfd9a64937022c57f77b2eccfc6d623ff8c1e900
Bowen Xu <maxxbw1992@gmail.com>
2018-11-14 20:37:56 +0800

    update time util

--- ProgramLanguage/Python/Lib/utils/time_util.py
+++ ProgramLanguage/Python/Lib/utils/time_util.py
@@ -9,12 +9,17 @@
 -------------------------------------------------
 """
 
-def time_record(fun):
-    import time
-    import datetime
-    print('start time : %s'%datetime.datetime.now())
-    fun()
-    print('end time : %s'%datetime.datetime.now())
+import time
+
+def time_record(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        for x in xrange(5000):
+            results = func(*args, **kwargs)
+        t2 = time.time()
+        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
+        return results
+    return wrapper
 
 @time_record
 def fun():
