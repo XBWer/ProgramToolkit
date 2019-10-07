7c246d4b41fadfa79b69533bbaf7120464cd0661
XBWer <maxxbw1992@gmail.com>
2019-10-07 10:33:28 +0800

    add cmd util

--- python/lib/utils/cmd_util.py
+++ python/lib/utils/cmd_util.py
@@ -1,11 +1,21 @@
 import subprocess
 
 
-def execute_cmd(cmd, working_dir):
+def execute_cmd_with_output(cmd, working_dir=None):
     try:
-        res = subprocess.check_output(cmd, shell=True, cwd=working_dir).decode("utf-8").strip()
+        res = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, cwd=working_dir)
     except Exception as e:
         print("Execute {} failed! cwd={}".format(cmd, working_dir))
         print(e)
         return None
     return res
+
+
+def execute_cmd_without_output(cmd, working_dir):
+    try:
+        subprocess.call(cmd, shell=True, cwd=working_dir)
+    except Exception as e:
+        print("Execute {} failed! cwd={}".format(cmd, working_dir))
+        print(e)
+        return False
+    return True
