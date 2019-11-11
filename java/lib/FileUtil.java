3e944bdef5063df41d34ad55d62ef86a2bc2334e
Bowen Xu <maxxbw1992@gmail.com>
2019-11-11 20:06:23 +0800

    Update FileUtil.java

--- java/lib/FileUtil.java
+++ java/lib/FileUtil.java
@@ -52,5 +52,114 @@ public class FileUtil {
 		}
 		return directoryToBeDeleted.delete();
 	}
+	
+	public static void copyFolder(Path srcDirStr, Path destDirStr) {
+		File source = new File(srcDirStr.toString());
+		File dest = new File(destDirStr.toString());
+		try {
+			FileUtils.copyDirectory(source, dest);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeStringToFile(String filePath, String dataStr) {
+		BufferedWriter bufferedWriter = null;
+		try {
+			File file = new File(filePath);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			Writer writer = new FileWriter(file);
+			bufferedWriter = new BufferedWriter(writer);
+			bufferedWriter.write(dataStr);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bufferedWriter != null)
+					bufferedWriter.close();
+			} catch (Exception e) {
+				System.out.println("Write file error!");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static String readStringFromFile(String filePath) {
+		try {
+			File file = new File(filePath);
+			if (file.isFile() && file.exists()) {
+				InputStreamReader read = new InputStreamReader(new FileInputStream(file));
+				BufferedReader bufferedReader = new BufferedReader(read);
+				StringBuilder stringBuilder = new StringBuilder();
+				String line = null;
+				String ls = System.getProperty("line.separator");
+				while ((line = bufferedReader.readLine()) != null) {
+					stringBuilder.append(line);
+					stringBuilder.append(ls);
+				}
+
+				// delete the last new line separator
+				stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+				bufferedReader.close();
+
+				String content = stringBuilder.toString();
+				return content;
+			} else {
+				System.out.println("File not found!");
+			}
+		} catch (Exception e) {
+			System.out.println("Read file error!");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<String> readFileToStrList(String filePath) {
+		ArrayList<String> strList = new ArrayList<String>();
+		try {
+			if (!new File(filePath).exists()) {
+				System.out.println("File not exists! " + filePath);
+				return null;
+			}
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			String line = reader.readLine();
+			while (line != null) {
+				strList.add(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("filePath : " + filePath);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println("filePath : " + filePath);
+			e.printStackTrace();
+		}
+		return strList;
+	}
+
+	public static void deleteDirectory(File directoryToBeDeleted) {
+		File[] allContents = directoryToBeDeleted.listFiles();
+		if (allContents != null) {
+			for (File file : allContents) {
+				deleteDirectory(file);
+			}
+		}
+		directoryToBeDeleted.delete();
+	}
+
+	public static void createFolder(String dirStr, Boolean deleteIfExist) {
+		File dir = new File(dirStr);
+		if (deleteIfExist && dir.exists()) {
+			deleteDirectory(dir);
+			dir.mkdirs();
+		} else if (!dir.exists()) {
+			dir.mkdirs();
+		}
+	}
 
 }
